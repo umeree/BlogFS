7e5ee545e41b78d8e83c1faacab0adbd2c040d26
umeree <umernadeam@gmail.com>
2023-03-07 20:41:01 +0500

    Changing in dessigns

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -11,6 +11,7 @@ function Login() {
                 <input type= "text" placeholder= "Enter your username" />
                 <input type= "password" placeholder= "Enter your password" />
                 <button>Login</button> 
+                <p>This is an error!</p>
                 <span>Don't you have any account? <Link to= "/register">Register</Link></span>
             </form>
         </div>
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+// import style from ".style.scss"
 function Register() {
     return (
-        <h2>This is register Page</h2>
+        <div className='auth'>
+            <h1>
+                Register
+            </h1>
+            <form>
+                <input required type= "text" placeholder= "Enter your username" />
+                <input required type= "text" placeholder= "Enter your email" />
+                <input required type= "password" placeholder= "Enter your password" />
+                <button>Register</button> 
+                <p>This is an error!</p>
+                <span>Don't you have any account? <Link to= "/login">Login</Link></span>
+            </form>
+        </div>
     )
 }
 
